1006e178fa36015e4a0899e002a919f1e3743039
alirezamojiri <108415196+alirezamojiri@users.noreply.github.com>
2023-02-02 06:19:06 +0330

    new test package v

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "443ced3ee0aadeef"; }
+/******/ 		__webpack_require__.h = function() { return "1d77bb44cdaa1df8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.a14d59ef6c6afc86.hot-update.js
+++ .next/static/webpack/webpack.a14d59ef6c6afc86.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "25da3fc857c85088"; }
+/******/ 	__webpack_require__.h = function() { return "1d77bb44cdaa1df8"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
